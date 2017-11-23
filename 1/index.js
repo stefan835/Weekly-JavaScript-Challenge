d4fad692e7b9901ea8ab90fe81b26516e9faf48d
Piotr Stefanowicz <stefan8357@gmail.com>
2017-11-23 10:48:07 +0100

    addig fixes allowing script working correctly on multiple browsers

--- 1/index.js
+++ 1/index.js
@@ -1,3 +1,9 @@
+const checkValidity = (input) => {
+  Number.isNaN(input) || input < 0 || input > 10000 ? displayErrorMessage() : document.querySelector('.result').textContent = findPrimeNumbers(input)
+};
+const displayErrorMessage = () => {
+  document.querySelector('.result').textContent = "Podana wartość nie spełnia danych kryteriów: Nie jest liczbą całkowitą, jest mniejsza od 0 lub jest większa od 10000"
+};
 const drawNumbersArray = (integer) => {
   let resultArr = [];
   for (let i = 2; i < integer; i++) {
@@ -25,5 +31,5 @@ const findPrimeNumbers = (integer) => {
 document.querySelector('#prime-number').addEventListener('submit', (event) => {
   event.preventDefault();
   const integerInput = parseInt(event.target.children[0].value);
-  document.querySelector('.result').textContent = findPrimeNumbers(integerInput)
+  checkValidity(integerInput);
 });
